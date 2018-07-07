9acb0d108032ef87fdad77e9d427a720fd0ef873
Matheus Costa <matheuscosta2004@hotmail.com>
2018-07-06 22:08:16 -0300

    Added 1st function

--- ValidadorCPF/src/validadorcpf/ValidadorCPF.java
+++ ValidadorCPF/src/validadorcpf/ValidadorCPF.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package validadorcpf;
 
 /**
- *
- * @author matheus
+ * @author Matheus
  */
 public class ValidadorCPF {
 
-    /**
-     * @param args the command line arguments
-     */
+    //insere o número do CPF (com ou sem verificadores) em um vetor, com cada dígito em uma posição
+    //o 1º parâmetro é o CPF, o 2º é a quantidade de dígitos (9-sem verificadores; 11-com verificadores);
+    public static int[] preencheVetor(long num, int length) {
+        int vet[] = new int[length];
+        String n = String.valueOf(num);
+        for (int i = 0; i < vet.length; i++) {
+            vet[i] = Integer.parseInt(String.valueOf(n.charAt(i)));
+        }
+        return vet;
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
